58675f1d8ceafb9e7457519f060b61564a614f75
kwklly <yeonlee@i.ewha.ac.kr>
2021-05-12 00:50:27 +0900

    :sparkles: Add tokenData function

--- src/index.js
+++ src/index.js
@@ -11,10 +11,19 @@ import {MintCluster} from './clusters/mint-cluster'
 import {TransferCluster} from './clusters/transfer-cluster'
 import {useCurrentUser} from './hooks/current-user'
 
+import {TokenCluster} from './clusters/token-cluster'
+
 function Transfer() {
   const cu = useCurrentUser()
   return (
     <TransferCluster address={cu.addr} />
+  )
+}
+
+function Token() {
+  const cu = useCurrentUser()
+  return (
+    <TokenCluster address={cu.addr} />
   )
 }
 
@@ -27,6 +36,7 @@ ReactDOM.render(
 
       <MintCluster />
       <Transfer />
+      <Token />
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
